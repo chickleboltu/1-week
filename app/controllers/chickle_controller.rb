144db06573f3d19aaa884d01434463287268c44a
TrickHunter <trickhunter.tk@gmail.com>
2018-03-26 09:41:08 +0000

    first web app

---
+++ app/controllers/chickle_controller.rb
@@ -0,0 +1,5 @@
+class ChickleController < ActionController::Base
+  protect_from_forgery with: :exception
+  def chick
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+    get '/chickle/chick' , to: 'chickle#chick'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
